a14c675ea7324d62c22fc3c163cca2e66bd12550
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-12-12 10:59:45 +0100

    Fix SerializableDateTimeTest

--- tests/Infrastructure/ValueObject/Time/SerializableDateTimeTest.php
+++ tests/Infrastructure/ValueObject/Time/SerializableDateTimeTest.php
@@ -4,7 +4,6 @@ namespace App\Tests\Infrastructure\ValueObject\Time;
 
 use App\Infrastructure\Serialization\Json;
 use App\Infrastructure\ValueObject\Time\SerializableDateTime;
-use App\Infrastructure\ValueObject\Time\SerializableTimezone;
 use PHPUnit\Framework\TestCase;
 
 class SerializableDateTimeTest extends TestCase
@@ -12,22 +11,22 @@ class SerializableDateTimeTest extends TestCase
     public function testFromString(): void
     {
         $this->assertEquals(
-            new \DateTimeImmutable('2023-10-05 10:22:22', SerializableTimezone::default()),
-            SerializableDateTime::fromString('2023-10-05 10:22:22', SerializableTimezone::default())
+            new \DateTimeImmutable('2023-10-05 10:22:22'),
+            SerializableDateTime::fromString('2023-10-05 10:22:22')
         );
     }
 
     public function testFromTimeStamp(): void
     {
         $this->assertEquals(
-            new \DateTimeImmutable('2023-10-05 20:56:31', SerializableTimezone::default()),
-            SerializableDateTime::fromTimestamp('1696532191', SerializableTimezone::default())
+            new \DateTimeImmutable('2023-10-05 18:56:31'),
+            SerializableDateTime::fromTimestamp('1696532191')
         );
     }
 
     public function testSerialize(): void
     {
-        $date = SerializableDateTime::fromString('2023-10-05 10:22:22', SerializableTimezone::default());
+        $date = SerializableDateTime::fromString('2023-10-05 10:22:22');
         $this->assertEquals(
             Json::encode($date),
             Json::encode((string) $date),
@@ -37,8 +36,8 @@ class SerializableDateTimeTest extends TestCase
     public function testToUtc(): void
     {
         $this->assertEquals(
-            SerializableDateTime::fromString('2023-10-05 8:22:22', SerializableTimezone::UTC()),
-            SerializableDateTime::fromString('2023-10-05 10:22:22', SerializableTimezone::default())->toUtc(),
+            SerializableDateTime::fromString('2023-10-05 10:22:22'),
+            SerializableDateTime::fromString('2023-10-05 10:22:22')->toUtc(),
         );
     }
 }
