c1dfd6a262cea4e643dc1f587bd3251ee93fc064
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-12-15 18:05:29 +0100

    Way better setup

--- public/app.php
+++ public/app.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 // This is the index that will be used to bootstrap the actual app in /build.
-if (file_exists(__DIR__.'/html/index.html')) {
-    echo file_get_contents(__DIR__.'/html/index.html');
+$index = dirname(__DIR__).'/build/html/index.html';
+if (file_exists($index)) {
+    echo file_get_contents($index);
 
     exit(0);
 }
