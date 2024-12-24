b56ff5ef59ace2fca901b80812bd522ad636ac6f
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-12-24 12:30:41 +0100

    ISSUE-33: Bump app version

--- src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
+++ src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
@@ -184,7 +184,7 @@ final readonly class BuildHtmlVersionCommandHandler implements CommandHandler
                 'totalPhotoCount' => count($allImages),
                 'lastUpdate' => $now,
                 'athleteId' => $athleteId,
-                'currentAppVersion' => 'v0.1.10',
+                'currentAppVersion' => 'v0.1.11',
                 'latestAppVersion' => $this->gitHub->getRepoLatestRelease('robiningelbrecht/strava-statistics'),
             ]),
         );
