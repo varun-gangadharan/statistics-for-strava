a7aaadb6dc3469dbddb1f96c695ba0e7f1e22f51
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2024-12-29 19:50:13 +0100

    ISSUE-3: Move app version to constant

--- src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
+++ src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
@@ -62,6 +62,8 @@ use Twig\Environment;
 
 final readonly class BuildHtmlVersionCommandHandler implements CommandHandler
 {
+    private const string APP_VERSION = 'v0.1.14';
+
     public function __construct(
         private ActivityRepository $activityRepository,
         private ChallengeRepository $challengeRepository,
@@ -179,7 +181,7 @@ final readonly class BuildHtmlVersionCommandHandler implements CommandHandler
                 'totalPhotoCount' => count($allImages),
                 'lastUpdate' => $now,
                 'athleteId' => $athleteId,
-                'currentAppVersion' => 'v0.1.14',
+                'currentAppVersion' => self::APP_VERSION,
                 'latestAppVersion' => $this->gitHub->getRepoLatestRelease('robiningelbrecht/strava-statistics'),
             ]),
         );
