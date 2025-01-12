939515ca2dcb30bb87f560a61c844284e3533cbc
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-01-12 11:06:48 +0100

    ISSUE-104: Readme

---
+++ tests/Infrastructure/Notification/Ntfy/LiveNtfyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Infrastructure\Notification\Ntfy;
+
+use App\Infrastructure\Notification\Ntfy\LiveNtfy;
+use PHPUnit\Framework\TestCase;
+
+class LiveNtfyTest extends TestCase
+{
+
+    public function testSendNotification(): void
+    {
+
+    }
+}
