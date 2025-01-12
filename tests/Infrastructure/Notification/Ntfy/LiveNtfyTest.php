5944b4f2853368c6effefd137fbf8dba27882185
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-01-12 11:16:33 +0100

    ISSUE-104: Add tests

--- tests/Infrastructure/Notification/Ntfy/LiveNtfyTest.php
+++ tests/Infrastructure/Notification/Ntfy/LiveNtfyTest.php
@@ -3,13 +3,53 @@
 namespace App\Tests\Infrastructure\Notification\Ntfy;
 
 use App\Infrastructure\Notification\Ntfy\LiveNtfy;
+use App\Infrastructure\Notification\Ntfy\Ntfy;
+use App\Infrastructure\Notification\Ntfy\NtfyUrl;
+use App\Infrastructure\ValueObject\String\Url;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
 class LiveNtfyTest extends TestCase
 {
+    use MatchesSnapshots;
+
+    private Ntfy $ntfy;
+    private MockObject $client;
 
     public function testSendNotification(): void
     {
+        $this->client
+            ->expects($this->once())
+            ->method('request')
+            ->willReturnCallback(function (string $method, string $path, array $options) {
+                $this->assertEquals('POST', $method);
+                $this->assertEquals('https://ntfy.com/some-topic', $path);
+                $this->assertMatchesJsonSnapshot($options);
 
+                return new Response(200, []);
+            });
+
+        $this->ntfy->sendNotification(
+            title: 'The title',
+            message: 'The message',
+            tags: ['+1'],
+            click: null,
+            icon: Url::fromString('https://raw.githubusercontent.com/robiningelbrecht/strava-statistics/master/public/assets/images/logo.png')
+        );
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = $this->createMock(Client::class);
+
+        $this->ntfy = new LiveNtfy(
+            $this->client,
+            NtfyUrl::fromString('https://ntfy.com/some-topic'),
+        );
     }
 }
