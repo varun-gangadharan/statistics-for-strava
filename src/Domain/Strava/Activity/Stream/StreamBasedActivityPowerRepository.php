aa3ffab07e9d423ddcd4a6728b98348efdf1c115
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-01-15 11:51:33 +0100

    ISSUE-168: Improve error messages

--- src/Domain/Strava/Activity/Stream/StreamBasedActivityPowerRepository.php
+++ src/Domain/Strava/Activity/Stream/StreamBasedActivityPowerRepository.php
@@ -55,7 +55,8 @@ final class StreamBasedActivityPowerRepository implements ActivityPowerRepositor
                 try {
                     $athleteWeight = $this->athleteWeightRepository->find($activity->getStartDate())->getWeightInKg();
                 } catch (EntityNotFound) {
-                    throw new EntityNotFound(sprintf('Trying to calculate the relative power for activity "%s" on %s, but no corresponding athleteWeight was found. Make sure you configure the proper weights in your .env file', $activity->getName(), $activity->getStartDate()->format('Y-m-d')));
+                    throw new EntityNotFound(sprintf('Trying to calculate the relative power for activity "%s" on %s, but no corresponding athleteWeight was found. 
+                    Make sure you configure the proper weights in your .env file. Do not forgot to run the app:strava:import-data command after changing the weights', $activity->getName(), $activity->getStartDate()->format('Y-m-d')));
                 }
 
                 $relativePower = $athleteWeight->toFloat() > 0 ? round($bestAverageForTimeInterval / $athleteWeight->toFloat(), 2) : 0;
@@ -119,7 +120,8 @@ final class StreamBasedActivityPowerRepository implements ActivityPowerRepositor
             try {
                 $athleteWeight = $this->athleteWeightRepository->find($activity->getStartDate())->getWeightInKg();
             } catch (EntityNotFound) {
-                throw new EntityNotFound(sprintf('Trying to calculate the relative power for activity "%s" on %s, but no corresponding athleteWeight was found. Make sure you configure the proper weights in your .env file', $activity->getName(), $activity->getStartDate()->format('Y-m-d')));
+                throw new EntityNotFound(sprintf('Trying to calculate the relative power for activity "%s" on %s, but no corresponding athleteWeight was found. 
+                    Make sure you configure the proper weights in your .env file. Do not forgot to run the app:strava:import-data command after changing the weights', $activity->getName(), $activity->getStartDate()->format('Y-m-d')));
             }
 
             $relativePower = $athleteWeight->toFloat() > 0 ? round($bestAverageForTimeInterval / $athleteWeight->toFloat(), 2) : 0;
