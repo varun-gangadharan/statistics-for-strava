84ec4966b93cc7ce04d27223b8aec1833a755e07
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-01-30 21:04:20 +0100

    ISSUE-192: Bumped APP version

--- src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
+++ src/Domain/Strava/BuildHtmlVersion/BuildHtmlVersionCommandHandler.php
@@ -66,7 +66,7 @@ use Twig\Environment;
 
 final readonly class BuildHtmlVersionCommandHandler implements CommandHandler
 {
-    public const string APP_VERSION = 'v0.4.6';
+    public const string APP_VERSION = 'v0.4.7';
 
     public function __construct(
         private ActivityRepository $activityRepository,
