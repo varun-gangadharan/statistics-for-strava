47dcb941ec584ef8fdb5c5883191acf6b75b4401
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-02-24 09:35:11 +0100

    ISSUE-294: Make sure Trivia is cached

--- src/Domain/Strava/Trivia.php
+++ src/Domain/Strava/Trivia.php
@@ -11,9 +11,30 @@ final class Trivia
 {
     public static ?Trivia $instance = null;
 
+    private readonly int $totalKudosReceived;
+    private readonly Activity $firstActivity;
+    private readonly Activity $earliestActivity;
+    private readonly Activity $latestActivity;
+    private readonly Activity $longestActivity;
+    private readonly ?Activity $fastestRide;
+    private readonly Activity $activityWithMostElevation;
+    private readonly Activity $mostKudotedActivity;
+    private readonly Dates $mostConsecutiveDaysOfWorkingOut;
+
     private function __construct(
         private readonly Activities $activities,
     ) {
+        $this->totalKudosReceived = (int) $this->activities->sum(fn (Activity $activity) => $activity->getKudoCount());
+        $this->firstActivity = $this->determineFirstActivity();
+        $this->earliestActivity = $this->determineEarliestActivity();
+        $this->latestActivity = $this->determineLatestActivity();
+        $this->longestActivity = $this->determineLongestWorkout();
+        $this->fastestRide = $this->determineFastestRide();
+        $this->activityWithMostElevation = $this->determineActivityWithHighestElevation();
+        $this->mostKudotedActivity = $this->determineMostKudotedActivity();
+        $this->mostConsecutiveDaysOfWorkingOut = Dates::fromDates($this->activities->map(
+            fn (Activity $activity) => $activity->getStartDate(),
+        ))->getLongestConsecutiveDateRange();
     }
 
     public static function getInstance(Activities $activities): self
@@ -27,24 +48,50 @@ final class Trivia
 
     public function getTotalKudosReceived(): int
     {
-        return (int) $this->activities->sum(fn (Activity $activity) => $activity->getKudoCount());
+        return $this->totalKudosReceived;
     }
 
     public function getMostKudotedActivity(): Activity
     {
-        /** @var Activity $mostKudotedActivity */
-        $mostKudotedActivity = $this->activities->getFirst();
-        foreach ($this->activities as $activity) {
-            if ($activity->getKudoCount() < $mostKudotedActivity->getKudoCount()) {
-                continue;
-            }
-            $mostKudotedActivity = $activity;
-        }
-
-        return $mostKudotedActivity;
+        return $this->mostKudotedActivity;
     }
 
     public function getFirstActivity(): Activity
+    {
+        return $this->firstActivity;
+    }
+
+    public function getEarliestActivity(): Activity
+    {
+        return $this->earliestActivity;
+    }
+
+    public function getLatestActivity(): Activity
+    {
+        return $this->latestActivity;
+    }
+
+    public function getLongestWorkout(): Activity
+    {
+        return $this->longestActivity;
+    }
+
+    public function getFastestRide(): ?Activity
+    {
+        return $this->fastestRide;
+    }
+
+    public function getActivityWithMostElevation(): Activity
+    {
+        return $this->activityWithMostElevation;
+    }
+
+    public function getMostConsecutiveDaysOfWorkingOut(): Dates
+    {
+        return $this->mostConsecutiveDaysOfWorkingOut;
+    }
+
+    private function determineFirstActivity(): Activity
     {
         /** @var Activity $fistActivity */
         $fistActivity = $this->activities->getFirst();
@@ -58,7 +105,7 @@ final class Trivia
         return $fistActivity;
     }
 
-    public function getEarliestActivity(): Activity
+    private function determineEarliestActivity(): Activity
     {
         /** @var Activity $earliestActivity */
         $earliestActivity = $this->activities->getFirst();
@@ -72,7 +119,7 @@ final class Trivia
         return $earliestActivity;
     }
 
-    public function getLatestActivity(): Activity
+    private function determineLatestActivity(): Activity
     {
         /** @var Activity $latestActivity */
         $latestActivity = $this->activities->getFirst();
@@ -86,7 +133,7 @@ final class Trivia
         return $latestActivity;
     }
 
-    public function getLongestWorkout(): Activity
+    private function determineLongestWorkout(): Activity
     {
         /** @var Activity $longestActivity */
         $longestActivity = $this->activities->getFirst();
@@ -100,7 +147,7 @@ final class Trivia
         return $longestActivity;
     }
 
-    public function getFastestRide(): ?Activity
+    private function determineFastestRide(): ?Activity
     {
         $bikeActivities = $this->activities->filterOnActivityType(ActivityType::RIDE);
 
@@ -119,7 +166,7 @@ final class Trivia
         return $fastestActivity;
     }
 
-    public function getActivityWithHighestElevation(): Activity
+    private function determineActivityWithHighestElevation(): Activity
     {
         /** @var Activity $mostElevationActivity */
         $mostElevationActivity = $this->activities->getFirst();
@@ -133,10 +180,17 @@ final class Trivia
         return $mostElevationActivity;
     }
 
-    public function getMostConsecutiveDaysOfWorkingOut(): Dates
+    private function determineMostKudotedActivity(): Activity
     {
-        return Dates::fromDates($this->activities->map(
-            fn (Activity $activity) => $activity->getStartDate(),
-        ))->getLongestConsecutiveDateRange();
+        /** @var Activity $mostKudotedActivity */
+        $mostKudotedActivity = $this->activities->getFirst();
+        foreach ($this->activities as $activity) {
+            if ($activity->getKudoCount() < $mostKudotedActivity->getKudoCount()) {
+                continue;
+            }
+            $mostKudotedActivity = $activity;
+        }
+
+        return $mostKudotedActivity;
     }
 }
