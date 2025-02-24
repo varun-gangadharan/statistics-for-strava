b5aac5699b26557ae7c14de3992840af11cb414a
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-02-24 13:14:47 +0100

    ISSUE-302: Bumped app version

--- src/Domain/App/AppVersion.php
+++ src/Domain/App/AppVersion.php
@@ -8,7 +8,7 @@ final readonly class AppVersion
 {
     private const int MAJOR = 0;
     private const int MINOR = 4;
-    private const int PATCH = 17;
+    private const int PATCH = 18;
 
     public static function getSemanticVersion(): string
     {
