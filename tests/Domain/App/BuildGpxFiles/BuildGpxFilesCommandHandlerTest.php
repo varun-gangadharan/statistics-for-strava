52140cf85497ae19a543dc10eb3eaab55e9220bf
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-02-28 17:24:30 +0100

    ISSUE-290: Added test for usecase

--- tests/Domain/App/BuildGpxFiles/BuildGpxFilesCommandHandlerTest.php
+++ tests/Domain/App/BuildGpxFiles/BuildGpxFilesCommandHandlerTest.php
@@ -5,11 +5,24 @@ namespace App\Tests\Domain\App\BuildGpxFiles;
 use App\Domain\App\BuildGpxFiles\BuildGpxFiles;
 use App\Infrastructure\CQRS\DomainCommand;
 use App\Tests\Domain\App\BuildAppFilesTestCase;
+use League\Flysystem\FilesystemOperator;
 
 class BuildGpxFilesCommandHandlerTest extends BuildAppFilesTestCase
 {
     protected function getDomainCommand(): DomainCommand
     {
         return new BuildGpxFiles();
+    }
+
+    public function testHandleDuplicates(): void
+    {
+        $this->provideFullTestSet();
+
+        $this->commandBus->dispatch($this->getDomainCommand());
+        $this->commandBus->dispatch($this->getDomainCommand());
+
+        /** @var \App\Tests\Infrastructure\FileSystem\SpyFileSystem $fileSystem */
+        $fileSystem = $this->getContainer()->get(FilesystemOperator::class);
+        $this->assertFileSystemWrites($fileSystem->getWrites());
     }
 }
