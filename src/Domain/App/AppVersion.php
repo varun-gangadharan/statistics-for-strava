baaf268c7ab2f2576f2431da37925f55af29e426
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-03-10 07:28:41 +0100

    Bumped app version to v0.4.25

--- src/Domain/App/AppVersion.php
+++ src/Domain/App/AppVersion.php
@@ -8,7 +8,7 @@ final readonly class AppVersion
 {
     private const int MAJOR = 0;
     private const int MINOR = 4;
-    private const int PATCH = 24;
+    private const int PATCH = 25;
 
     public static function getSemanticVersion(): string
     {
