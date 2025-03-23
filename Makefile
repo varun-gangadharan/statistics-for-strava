b0a2fd0dd00dda4d3bbb2de2b3eee1a64c23d231
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-03-23 18:58:17 +0100

    ISSUE-434: Activity best efforts calculation

--- Makefile
+++ Makefile
@@ -25,20 +25,25 @@ console-symfony-profiler:
 	# We need to use the app container here, otherwise the profiler can't access the files through web requests.
 	docker compose exec app bin/console --profile -v $(arg)
 
+composer:
+	@make dcr cmd="composer $(arg)"
+
+download-database:
+	scp $(user)@$(server):/home/docker/stacks/strava-statistics/storage/database/strava.db ./storage/database/strava.db
+
+# Database migration helpers.
 migrate-diff:
 	@make console arg="doctrine:migrations:diff"
 
 migrate-run:
 	@make console arg="doctrine:migrations:migrate"
 
+# Translation helpers.
 translation-extract:
 	@make console arg="app:translations:extract"
 
 translation-debug:
 	@make console arg="debug:translation en_US"
-
-composer:
-	@make dcr cmd="composer $(arg)"
 
 # Code quality tools.
 phpunit:
@@ -59,7 +64,7 @@ csfix:
 delete-snapshots:
 	find . -name __snapshots__ -type d -prune -exec rm -rf {} \;
 
-# Helpers to manage app imports.
+# Helpers to build the app.
 app-import-data:
 	docker compose exec app bin/console app:strava:import-data
 
--- src/Domain/Strava/Activity/BestEffort/CalculateBestActivityEfforts/CalculateBestActivityEffortsCommandHandler.php
+++ src/Domain/Strava/Activity/BestEffort/CalculateBestActivityEfforts/CalculateBestActivityEffortsCommandHandler.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace App\Domain\Strava\Activity\BestEffort\CalculateBestActivityEfforts;
 
+use App\Domain\Strava\Activity\ActivityRepository;
 use App\Domain\Strava\Activity\BestEffort\ActivityBestEffort;
 use App\Domain\Strava\Activity\BestEffort\ActivityBestEffortRepository;
-use App\Domain\Strava\Activity\SportType\SportType;
 use App\Domain\Strava\Activity\Stream\ActivityStreamRepository;
 use App\Domain\Strava\Activity\Stream\StreamType;
 use App\Infrastructure\CQRS\Command;
 use App\Infrastructure\CQRS\CommandHandler;
+use App\Infrastructure\Exception\EntityNotFound;
 
 final readonly class CalculateBestActivityEffortsCommandHandler implements CommandHandler
 {
     public function __construct(
+        private ActivityRepository $activityRepository,
         private ActivityBestEffortRepository $activityBestEffortRepository,
         private ActivityStreamRepository $activityStreamRepository,
     ) {
@@ -27,46 +29,51 @@ final readonly class CalculateBestActivityEffortsCommandHandler implements Comma
 
         $activityIdsWithoutBestEfforts = $this->activityBestEffortRepository->findActivityIdsWithoutBestEfforts();
 
+        $activityWithBestEffortsCalculatedCount = 0;
         foreach ($activityIdsWithoutBestEfforts as $activityId) {
-            $distanceStream = $this->activityStreamRepository->findOneByActivityAndStreamType($activityId, StreamType::DISTANCE);
-            $timeStream = $this->activityStreamRepository->findOneByActivityAndStreamType($activityId, StreamType::TIME);
+            try {
+                $distanceStream = $this->activityStreamRepository->findOneByActivityAndStreamType($activityId, StreamType::DISTANCE);
+                $timeStream = $this->activityStreamRepository->findOneByActivityAndStreamType($activityId, StreamType::TIME);
+            } catch (EntityNotFound) {
+                continue;
+            }
+            ++$activityWithBestEffortsCalculatedCount;
 
+            $activity = $this->activityRepository->find($activityId);
             $distances = $distanceStream->getData();
             $time = $timeStream->getData();
 
-            foreach (SportType::cases() as $sportType) {
-                if (!$distancesForBestEfforts = $sportType->getDistancesForBestEffortCalculation()) {
+            if (!$distancesForBestEfforts = $activity->getSportType()->getDistancesForBestEffortCalculation()) {
+                continue;
+            }
+
+            foreach ($distancesForBestEfforts as $distance) {
+                $n = count($distances);
+                $fastestTime = PHP_INT_MAX;
+                $startIdx = 0;
+
+                for ($endIdx = 0; $endIdx < $n; ++$endIdx) {
+                    while ($startIdx < $endIdx && ($distances[$endIdx] - $distances[$startIdx]) >= $distance->toInt()) {
+                        $fastestTime = min($fastestTime, $time[$endIdx] - $time[$startIdx]);
+                        ++$startIdx;
+                    }
+                }
+
+                if (PHP_INT_MAX === $fastestTime) {
+                    // No fastest time for this distance.
                     continue;
                 }
 
-                foreach ($distancesForBestEfforts as $distance) {
-                    $n = count($distances);
-                    $fastestTime = PHP_INT_MAX;
-                    $startIdx = 0;
-
-                    for ($endIdx = 0; $endIdx < $n; ++$endIdx) {
-                        while ($startIdx < $endIdx && ($distances[$endIdx] - $distances[$startIdx]) >= $distance->toInt()) {
-                            $fastestTime = min($fastestTime, $time[$endIdx] - $time[$startIdx]);
-                            ++$startIdx;
-                        }
-                    }
-
-                    if (PHP_INT_MAX === $fastestTime) {
-                        // No fastest time for this distance.
-                        continue;
-                    }
-
-                    $this->activityBestEffortRepository->add(
-                        ActivityBestEffort::create(
-                            activityId: $activityId,
-                            distanceInMeter: $distance,
-                            sportType: $sportType,
-                            timeInSeconds: $fastestTime,
-                        )
-                    );
-                }
+                $this->activityBestEffortRepository->add(
+                    ActivityBestEffort::create(
+                        activityId: $activityId,
+                        distanceInMeter: $distance,
+                        sportType: $activity->getSportType(),
+                        timeInSeconds: $fastestTime,
+                    )
+                );
             }
         }
-        $command->getOutput()->writeln(sprintf('  => Calculated best efforts for %d activities', count($activityIdsWithoutBestEfforts)));
+        $command->getOutput()->writeln(sprintf('  => Calculated best efforts for %d activities', $activityWithBestEffortsCalculatedCount));
     }
 }
