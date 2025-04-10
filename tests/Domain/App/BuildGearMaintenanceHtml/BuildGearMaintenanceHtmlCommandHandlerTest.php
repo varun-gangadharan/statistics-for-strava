ae59e92e5e7b97e1be2b063e67fbb3009279eaec
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-04-10 07:53:05 +0200

    BuildGearMaintenanceHtmlCommandHandler

---
+++ tests/Domain/App/BuildGearMaintenanceHtml/BuildGearMaintenanceHtmlCommandHandlerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Domain\App\BuildGearMaintenanceHtml;
+
+use App\Domain\App\BuildGearMaintenanceHtml\BuildGearMaintenanceHtmlCommandHandler;
+use PHPUnit\Framework\TestCase;
+
+class BuildGearMaintenanceHtmlCommandHandlerTest extends TestCase
+{
+
+}
