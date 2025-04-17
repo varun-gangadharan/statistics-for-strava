ed1425a38e1a2d66bdf0157a58e9dde825fac24e
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-04-17 16:29:52 +0200

    ISSUE-444: Render different rewind components

--- src/Domain/App/BuildRewindHtml/BuildRewindHtmlCommandHandler.php
+++ src/Domain/App/BuildRewindHtml/BuildRewindHtmlCommandHandler.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Domain\App\BuildRewindHtml;
 
+use App\Domain\Strava\Rewind\RewindItem;
 use App\Infrastructure\CQRS\Command;
 use App\Infrastructure\CQRS\CommandHandler;
 use League\Flysystem\FilesystemOperator;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 final readonly class BuildRewindHtmlCommandHandler implements CommandHandler
@@ -14,6 +16,7 @@ final readonly class BuildRewindHtmlCommandHandler implements CommandHandler
     public function __construct(
         private Environment $twig,
         private FilesystemOperator $buildStorage,
+        private TranslatorInterface $translator,
     ) {
     }
 
@@ -23,7 +26,102 @@ final readonly class BuildRewindHtmlCommandHandler implements CommandHandler
 
         $this->buildStorage->write(
             'rewind.html',
-            $this->twig->load('html/rewind.html.twig')->render(),
+            $this->twig->load('html/rewind/rewind.html.twig')->render([
+                'rewindItems' => [
+                    RewindItem::from(
+                        icon: 'calendar',
+                        title: $this->translator->trans('Daily activities'),
+                        subTitle: null,
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'tools',
+                        title: $this->translator->trans('Gear'),
+                        subTitle: $this->translator->trans('Total hours spent using gear'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'ruler',
+                        title: $this->translator->trans('Distance ranges'),
+                        subTitle: $this->translator->trans('Number of activities within a distance range'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'trophy',
+                        title: $this->translator->trans('Biggest activity'),
+                        subTitle: 'TODO: Name of the activity',
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'medal',
+                        title: $this->translator->trans('PRs'),
+                        subTitle: $this->translator->trans('PRs achieved per month'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'thumbs-up',
+                        title: $this->translator->trans('Socials'),
+                        subTitle: $this->translator->trans('Total kudos and comments received'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'rocket',
+                        title: $this->translator->trans('Distance'),
+                        subTitle: $this->translator->trans('Total distance per month'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'mountain',
+                        title: $this->translator->trans('Elevation'),
+                        subTitle: $this->translator->trans('Total elevation per month'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'muscle',
+                        title: $this->translator->trans('Activity count'),
+                        subTitle: $this->translator->trans('Number of activities per month'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'watch',
+                        title: $this->translator->trans('Total hours'),
+                        subTitle: $this->translator->trans('Total hours spent per sport type'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'fire',
+                        title: $this->translator->trans('Streaks'),
+                        subTitle: $this->translator->trans('Longest streaks'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'bed',
+                        title: $this->translator->trans('Rest days'),
+                        subTitle: $this->translator->trans('Rest days vs. active days'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'clock',
+                        title: $this->translator->trans('Start times'),
+                        subTitle: $this->translator->trans('Activity start times'),
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'image',
+                        title: $this->translator->trans('Photo'),
+                        subTitle: 'TODO: date of picture',
+                        content: ''
+                    ),
+                    RewindItem::from(
+                        icon: 'globe',
+                        title: $this->translator->trans('Activity locations'),
+                        subTitle: $this->translator->trans('Locations over the globe'),
+                        content: ''
+                    ),
+                ],
+
+                'activityLocations' => null,
+            ]),
         );
     }
 }
---
+++ src/Domain/Strava/Rewind/RewindItem.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Strava\Rewind;
+
+final readonly class RewindItem
+{
+    private function __construct(
+        private string $icon,
+        private string $title,
+        private ?string $subTitle,
+        private string $content,
+    ) {
+    }
+
+    public static function from(
+        string $icon,
+        string $title,
+        ?string $subTitle,
+        string $content,
+    ): self {
+        return new self(
+            icon: $icon,
+            title: $title,
+            subTitle: $subTitle,
+            content: $content
+        );
+    }
+
+    public function getIcon(): string
+    {
+        return $this->icon;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getSubTitle(): ?string
+    {
+        return $this->subTitle;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
