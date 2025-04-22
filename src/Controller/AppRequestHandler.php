6e3795f70d20f0b65092b995fac03e2d01244e03
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-04-22 15:30:41 +0200

    ISSUE-515: Remove app.php in favour of default Symfony routing

--- src/Controller/AppRequestHandler.php
+++ src/Controller/AppRequestHandler.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class AppRequestHandler
-{
+use League\Flysystem\FilesystemOperator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
+#[AsController]
+final readonly class AppRequestHandler
+{
+    public function __construct(
+        private FilesystemOperator $buildStorage,
+        private Environment $twig,
+    ) {
+    }
+
+    #[Route(path: '/{wildcard?}', methods: ['GET'])]
+    public function handle(Request $request): Response
+    {
+        if ($this->buildStorage->fileExists('index.html')) {
+            return new Response($this->buildStorage->read('index.html'), Response::HTTP_OK);
+        }
+
+        return new Response($this->twig->render('html/setup.html.twig'), Response::HTTP_OK);
+    }
 }
--- tests/Controller/AppRequestHandlerTest.php
+++ tests/Controller/AppRequestHandlerTest.php
@@ -3,9 +3,52 @@
 namespace App\Tests\Controller;
 
 use App\Controller\AppRequestHandler;
-use PHPUnit\Framework\TestCase;
+use App\Tests\ContainerTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use Symfony\Component\HttpFoundation\Request;
+use Twig\Environment;
 
-class AppRequestHandlerTest extends TestCase
+class AppRequestHandlerTest extends ContainerTestCase
 {
+    use MatchesSnapshots;
 
+    private AppRequestHandler $appRequestHandler;
+
+    public function testHandle(): void
+    {
+        /** @var \League\Flysystem\InMemory\InMemoryFilesystemAdapter $buildStorage */
+        $buildStorage = $this->getContainer()->get('build.storage');
+        $buildStorage->write('index.html', 'I am the index', []);
+
+        $this->assertMatchesHtmlSnapshot($this->appRequestHandler->handle(new Request(
+            query: [],
+            request: [],
+            attributes: [],
+            cookies: [],
+            files: [],
+            server: [],
+            content: [],
+        ))->getContent());
+    }
+
+    public function testHandleWhenNotBuilt(): void
+    {
+        $this->assertMatchesHtmlSnapshot($this->appRequestHandler->handle(new Request(
+            query: [],
+            request: [],
+            attributes: [],
+            cookies: [],
+            files: [],
+            server: [],
+            content: [],
+        ))->getContent());
+    }
+
+    protected function setUp(): void
+    {
+        $this->appRequestHandler = new AppRequestHandler(
+            $this->getContainer()->get('build.storage'),
+            $this->getContainer()->get(Environment::class),
+        );
+    }
 }
