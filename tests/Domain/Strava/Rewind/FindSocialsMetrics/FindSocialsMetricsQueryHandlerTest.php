5cf594085b861379abab3d5d07baacdc3b3b8396
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-04-23 18:40:03 +0200

    ISSUE-444: FindSocialsMetricsQueryHandler

---
+++ tests/Domain/Strava/Rewind/FindSocialsMetrics/FindSocialsMetricsQueryHandlerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Domain\Strava\Rewind\FindSocialsMetrics;
+
+use App\Domain\Strava\Rewind\FindSocialsMetrics\FindSocialsMetricsQueryHandler;
+use PHPUnit\Framework\TestCase;
+
+class FindSocialsMetricsQueryHandlerTest extends TestCase
+{
+
+}
