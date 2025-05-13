f03c3dda760011044d9e3ec4e0d8e0dcd2d39669
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-05-13 07:59:45 +0200

    Use AutoconfigureTag

--- src/Domain/Strava/Gear/Maintenance/Task/Progress/MaintenanceTaskProgressCalculation.php
+++ src/Domain/Strava/Gear/Maintenance/Task/Progress/MaintenanceTaskProgressCalculation.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Domain\Strava\Gear\Maintenance\Task\Progress;
 
 use App\Domain\Strava\Gear\Maintenance\Task\IntervalUnit;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag('app.maintenance_progress_calculation')]
 interface MaintenanceTaskProgressCalculation
 {
     public function supports(IntervalUnit $intervalUnit): bool;
--- src/Infrastructure/CQRS/Query/QueryHandler.php
+++ src/Infrastructure/CQRS/Query/QueryHandler.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\CQRS\Query;
 
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('app.query_handler')]
 interface QueryHandler
 {
     public function handle(Query $query): Response;
