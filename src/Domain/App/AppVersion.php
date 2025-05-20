e029948cd371a54acd45255228ad4949f53dcacf
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-05-20 20:03:48 +0200

    ISSUE #582: Updated APP version

--- src/Domain/App/AppVersion.php
+++ src/Domain/App/AppVersion.php
@@ -8,7 +8,7 @@ final readonly class AppVersion
 {
     private const int MAJOR = 1;
     private const int MINOR = 3;
-    private const int PATCH = 5;
+    private const int PATCH = 6;
 
     public static function getSemanticVersion(): string
     {
