85ad9d204582576578fd63af6bd63e8d65f1ab74
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-05-25 10:42:36 +0200

    ISSUE #594: Fix debug console command

--- src/Infrastructure/Serialization/Json.php
+++ src/Infrastructure/Serialization/Json.php
@@ -11,6 +11,11 @@ class Json
         return json_encode($value, JSON_THROW_ON_ERROR, max(1, $depth));
     }
 
+    public static function encodePretty(mixed $value, int $depth = 512): string
+    {
+        return json_encode($value, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT, max(1, $depth));
+    }
+
     public static function decode(string $json, bool $assoc = true, int $depth = 512): mixed
     {
         return json_decode($json ?: '', $assoc, max(1, $depth), JSON_THROW_ON_ERROR);
--- tests/Console/DebugEnvironmentConsoleCommandTest.php
+++ tests/Console/DebugEnvironmentConsoleCommandTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Console;
 
 use App\Console\DebugEnvironmentConsoleCommand;
+use App\Infrastructure\Config\AppConfig;
 use Spatie\Snapshots\MatchesSnapshots;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -23,6 +24,7 @@ class DebugEnvironmentConsoleCommandTest extends ConsoleCommandTestCase
 
         $this->assertStringContainsString('Please copy all this output into the description of the bug ticket', $commandTester->getDisplay());
         $this->assertStringContainsString('APP_VERSION', $commandTester->getDisplay());
+        $this->assertMatchesTextSnapshot($commandTester->getDisplay());
     }
 
     #[\Override]
@@ -30,7 +32,9 @@ class DebugEnvironmentConsoleCommandTest extends ConsoleCommandTestCase
     {
         parent::setUp();
 
-        $this->debugEnvironmentConsoleCommand = new DebugEnvironmentConsoleCommand();
+        $this->debugEnvironmentConsoleCommand = new DebugEnvironmentConsoleCommand(
+            $this->getContainer()->get(AppConfig::class),
+        );
     }
 
     protected function getConsoleCommand(): Command
