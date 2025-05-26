0272466143acf9a87459063dbae6de391d02b27e
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-05-26 12:38:10 +0200

    ISSUE #608: Simplify expression language provider

--- src/Infrastructure/DependencyInjection/AppExpressionLanguageProvider.php
+++ src/Infrastructure/DependencyInjection/AppExpressionLanguageProvider.php
@@ -9,10 +9,13 @@ use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
 
 final readonly class AppExpressionLanguageProvider implements ExpressionFunctionProviderInterface
 {
+    /**
+     * @return ExpressionFunction[]
+     */
     public function getFunctions(): array
     {
         return [
-            new ExpressionFunction('app_config', function ($appConfigKey): string {
+            new ExpressionFunction('app_config', function (string $appConfigKey): string {
                 return sprintf('$container->get("app.config")->get(%s)', $appConfigKey);
             }, function (array $variables, string $appConfigKey): string {
                 return $variables['container']->get('app.config')->get($appConfigKey);
